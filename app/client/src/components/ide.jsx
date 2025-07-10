0fd56982ffafa64e09a2c74c7307b77715845c3c
ugantarrajmanu <ugantarrajmanu@gmail.com>
2025-07-10 23:23:45 +0530

    update code

--- app/client/src/components/ide.jsx
+++ app/client/src/components/ide.jsx
@@ -6,12 +6,22 @@ export function Ide() {
   const [outputs, setOutputs] = useState(["\t\tWelcome to the YUlang!"]);
 
   const [code, setCode] = useState(
-    `arey yug a = "hello" string hai
+    `arey yug a = "Hello Butterflies" string hai
 
+yug i ko 0 se 3 tak lejana hai {
+    yug agar (i%2 == 0) hai to {
+        yug a likhna hai
+    } nahi to {
+        yug "Bye Butterflies" likhna hai
+    }
+}
 
-yug i ko 5 se 10 tak lejana hai {
-    yug "Hello Butterflies" likhna hai
-}`
+arey yug b = 0 int hai
+
+yug i ko 0 se 5 tak lejana hai {
+    b = b + 5 hai
+}
+yug b likhna hai`
   );
 
   const executeCode = async () => {
